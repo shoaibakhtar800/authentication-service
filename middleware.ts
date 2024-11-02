32c3155a2086baa04a1fe9d3fbd108df0b9cfe68
shoaibakhtar800 <cs08018akp@gmail.com>
2024-11-02 19:14:32 +0530

    2: fixed middleware issue

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
 
 import authConfig from "@/auth.config";
 import { 
@@ -7,10 +7,11 @@ import {
     authRoutes, 
     publicRoutes 
 } from "@/routes";
+import { NextRequest } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth((req: NextRequest & { auth: Session | null }): Response | void => {
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
 
@@ -19,21 +20,21 @@ export default auth((req) => {
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
     if(isApiAuthRoute) {
-        return null;
+        return;
     }
 
     if (isAuthRoute) {
         if (isLoggedIn) {
             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
         }
-        return null;
+        return;
     }
 
     if(!isLoggedIn && !isPublicRoute) {
         return Response.redirect(new URL("/auth/login", nextUrl));
     }
 
-    return null;
+    return;
 });
 
 export const config = {
